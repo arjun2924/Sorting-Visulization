c65a0a49cb8d38de3be08b31a9dd78a3034fb7aa
soham thakur <124246472+arjun2924@users.noreply.github.com>
2024-06-12 13:08:16 +0530

    update

---
+++ beep.js
@@ -0,0 +1,12 @@
+a = new AudioContext() // browsers limit the number of concurrent audio contexts, so you better re-use'em
+
+function beep(vol, freq, duration) {
+    console.log({ vol, freq, duration })
+    v = a.createOscillator()
+    u = a.createGain()
+    v.connect(u)
+    v.frequency.value = freq
+    v.type = "square"
+    v.start(a.currentTime)
+    v.stop(a.currentTime + duration * 0.001)
+}
